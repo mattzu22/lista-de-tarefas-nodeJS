caa0f03d9e64953f826335105866c28e1cee3fa9
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2023-11-03 22:26:32 -0300

    rota para deletar tarefa alterada
    adicionado o redirect para a rota inicial

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
-app.use( bodyParser.json() );       
-app.use(bodyParser.urlencoded({     
-  extended: true
-})); 
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '/views'));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "/views"));
+
+let tarefas = ["Arrumar o quarto", "Comprar no supermercado"];
 
 
-let tarefas = ['Arrumar o quarto','Comprar no supermercado'];
-
-app.post('/',(req,res)=>{
-    tarefas.push(req.body.tarefa);
-    res.render('index',{tarefasList:tarefas});
-})
-
-app.get('/',(req,res)=>{
-
-    res.render('index',{tarefasList:tarefas});
-
+app.get("/", (req, res) => {
+  res.render("index", { tarefasList: tarefas });
 });
 
-app.get('/deleter/:id',(req,res)=>{
-    tarefas = tarefas.filter(function(val,index){
-            if(index != req.params.id){
-                return val;
-            }
-    })
-    res.render('index',{tarefasList:tarefas});
-})
+app.post("/", (req, res) => {
+  tarefas.push(req.body.tarefa);
+  res.render("index", { tarefasList: tarefas });
+});
 
-app.listen(5000,()=>{
-    console.log('server rodando!');
-})
+app.get("/deleter/:id", (req, res) => {
+  tarefas = tarefas.filter(function (val, index) {
+    if (index != req.params.id) {
+      return val;
+    }
+  });
+  res.redirect("/")
+});
+
+const PORT = 3333;
+
+app.listen(PORT, () => {
+  console.log("server rodando!");
+});
